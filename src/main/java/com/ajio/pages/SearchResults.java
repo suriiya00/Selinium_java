d9f1f87976d9601fe46aeb9e16aabb625a3b0db3
suriiya00 <131760964+suriiya00@users.noreply.github.com>
2025-11-03 15:22:07 +0530

    Refactor SearchResults class for improved element handling
    Updated WebDriverWait duration and refactored clickFirstProduct method to directly locate the first product.

--- src/main/java/com/ajio/pages/SearchResults.java
+++ src/main/java/com/ajio/pages/SearchResults.java
@@ -1,34 +1,40 @@
 package com.ajio.pages;
-
+ 
 import org.openqa.selenium.By;
-import org.openqa.selenium.JavascriptExecutor;
+
 import org.openqa.selenium.WebDriver;
+
 import org.openqa.selenium.WebElement;
+
 import org.openqa.selenium.support.ui.ExpectedConditions;
+
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.time.Duration;
-
+ 
 public class SearchResults {
 
     WebDriver driver;
+
     WebDriverWait wait;
-
-    private By firstProduct = By.cssSelector(".rilrtl-products-list__item");
-
+ 
     public SearchResults(WebDriver driver) {
+
         this.driver = driver;
-        this.wait = new WebDriverWait(driver, Duration.ofSeconds(20));
+
+        this.wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+
+    }
+ 
+    public void clickFirstProduct() {
+
+        WebElement firstProduct = wait.until(ExpectedConditions.elementToBeClickable(
+
+            By.cssSelector(".rilrtl-products-list__item")));
+
+        firstProduct.click();
+
     }
 
-    public void clickFirstProduct() {
-        wait.until(ExpectedConditions.presenceOfElementLocated(firstProduct));
-        
-        WebElement product = wait.until(ExpectedConditions.elementToBeClickable(firstProduct));
-        
-        // Scroll into view for safety
-        ((JavascriptExecutor) driver).executeScript("arguments[0].scrollIntoView(true);", product);
-        
-        product.click();
-    }
 }
+ 
