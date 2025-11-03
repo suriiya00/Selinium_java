0b34df1f7bc1c82903c9dc201b73a7fdcfb209c5
suriiya00 <131760964+suriiya00@users.noreply.github.com>
2025-11-03 15:03:37 +0530

    Update ProductPage.java

--- src/main/java/com/ajio/pages/ProductPage.java
+++ src/main/java/com/ajio/pages/ProductPage.java
@@ -1,60 +1,49 @@
 package com.ajio.pages;
- 
+
 import org.openqa.selenium.By;
-
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
-
 import org.openqa.selenium.WebElement;
-
 import org.openqa.selenium.support.ui.ExpectedConditions;
-
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.time.Duration;
- 
+
 public class ProductPage {
 
     WebDriver driver;
-
     WebDriverWait wait;
- 
+
     public ProductPage(WebDriver driver) {
-
         this.driver = driver;
-
-        this.wait = new WebDriverWait(driver, Duration.ofSeconds(10));
-
+        this.wait = new WebDriverWait(driver, Duration.ofSeconds(20));
     }
- 
+
     public void selectSize(String size) {
-
-        WebElement sizeOption = wait.until(ExpectedConditions.elementToBeClickable(
-
-            By.xpath("//span[text()='" + size + "']")));
-
-        sizeOption.click();
-
+        By sizeOption = By.xpath("//span[text()='" + size + "']");
+        WebElement sizeBtn = wait.until(ExpectedConditions.elementToBeClickable(sizeOption));
+        ((JavascriptExecutor) driver).executeScript("arguments[0].scrollIntoView(true);", sizeBtn);
+        sizeBtn.click();
     }
- 
+
     public void addToCart() {
-
-        WebElement addToBagBtn = wait.until(ExpectedConditions.elementToBeClickable(
-
-            By.xpath("//span[text()='ADD TO BAG']")));
-
-        addToBagBtn.click();
-
+        By addToBagBtn = By.xpath("//span[text()='ADD TO BAG']");
+        WebElement btn = wait.until(ExpectedConditions.elementToBeClickable(addToBagBtn));
+        try {
+            btn.click();
+        } catch (Exception e) {
+            ((JavascriptExecutor) driver).executeScript("arguments[0].click();", btn);
+        }
     }
- 
+
     public void addToWishlist() {
-
-        WebElement wishlistIcon = wait.until(ExpectedConditions.elementToBeClickable(
-
-            By.cssSelector("a[href*='wishlist']")));
-
-        wishlistIcon.click();
-
+        By wishlistIcon = By.cssSelector("a[href*='wishlist']");
+        WebElement icon = wait.until(ExpectedConditions.visibilityOfElementLocated(wishlistIcon));
+        ((JavascriptExecutor) driver).executeScript("arguments[0].scrollIntoView(true);", icon);
+        try {
+            icon.click();
+        } catch (Exception e) {
+            ((JavascriptExecutor) driver).executeScript("arguments[0].click();", icon);
+        }
     }
-
 }
- 
