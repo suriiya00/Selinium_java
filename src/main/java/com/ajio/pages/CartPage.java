c658eafb6b87ffdf90bb2ed550fb180f04cb3839
suriiya00 <131760964+suriiya00@users.noreply.github.com>
2025-11-03 15:04:03 +0530

    Refactor CartPage methods for better functionality

--- src/main/java/com/ajio/pages/CartPage.java
+++ src/main/java/com/ajio/pages/CartPage.java
@@ -1,50 +1,33 @@
 package com.ajio.pages;
- 
+
 import org.openqa.selenium.By;
-
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
-
 import org.openqa.selenium.WebElement;
-
 import org.openqa.selenium.support.ui.ExpectedConditions;
-
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.time.Duration;
- 
+
 public class CartPage {
 
     WebDriver driver;
-
     WebDriverWait wait;
- 
+
     public CartPage(WebDriver driver) {
-
         this.driver = driver;
-
-        this.wait = new WebDriverWait(driver, Duration.ofSeconds(10));
-
+        this.wait = new WebDriverWait(driver, Duration.ofSeconds(20));
     }
- 
+
     public void goToBag() {
-
-        WebElement bagIcon = wait.until(ExpectedConditions.elementToBeClickable(
-
-            By.cssSelector("a[href*='cart']")));
-
+        WebElement bagIcon = wait.until(ExpectedConditions.elementToBeClickable(By.cssSelector("a[href*='cart']")));
         bagIcon.click();
-
     }
- 
+
     public void proceedToCheckout() {
-
-        WebElement proceedBtn = wait.until(ExpectedConditions.elementToBeClickable(
-
-            By.xpath("//span[text()='PROCEED TO CHECKOUT']")));
-
-        proceedBtn.click();
-
+        WebElement checkoutBtn = wait.until(ExpectedConditions.visibilityOfElementLocated(
+                By.xpath("//span[text()='PROCEED TO CHECKOUT']")));
+        ((JavascriptExecutor) driver).executeScript("arguments[0].scrollIntoView(true);", checkoutBtn);
+        checkoutBtn.click();
     }
-
 }
- 
